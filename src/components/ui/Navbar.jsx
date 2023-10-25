4531bed653661fb684e3c748a5fc03f688ff9e3b
Abu Siam <siamcse01@gmail.com>
2023-10-25 11:38:44 +0600

    add: navbar

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const linkClass = `transition hover:text-orange-600 duration-300 text-gray-700`;
+    return (
+        <nav className='w-full py-3'>
+            <div className="container mx-auto flex justify-between items-center">
+                <Link to='/' className="text-2xl  text-gray-700 font-bold font-['Chivo_Mono']">Task<span className="text-primary">Muslin</span></Link>
+                <div className="flex gap-3 text-white">
+                    <Link className={linkClass} to="/">Home </Link>
+                    <Link className={linkClass} to="/books">Products</Link>
+                    <Link className={linkClass} to="/about">About</Link>
+                    <Link className={linkClass} to="/login">Login</Link>
+                    <Link className={linkClass} to="/signup">SignUp</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import MainLayout from "../components/layout/MainLayout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home/>
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
     }
 ])
 
