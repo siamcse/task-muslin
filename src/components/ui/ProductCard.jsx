11e5a94df5cc21264808d7e10a21163c8dd43d0a
Abu Siam <siamcse01@gmail.com>
2023-10-25 13:29:08 +0600

    add: fakestore api

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { StarIcon } from '@heroicons/react/24/solid'
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { title, price, description, image, rating } = product;
     return (
         <div className='w-64 h-full border-2 border-gray rounded-lg'>
             <div className='px-8 py-6'>
                 <div className='h-64 flex justify-center'>
-                    <img src={'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'} alt="" className='rounded max-h-56' />
+                    <img src={image} alt="" className='rounded max-h-56' />
                 </div>
                 <div className='mt-2'>
-                    <h1 title='Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops' className='font-light truncate '><span className='font-medium'>Brand,</span> Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-                    <p>Rate</p>
-                    <h1 className='text-2xl text-primary py-2'>Tk. 109</h1>
+                    <h1 title={title} className='font-light truncate '><span className='font-medium'>Brand,</span> {title}</h1>
+                    <p className='py-2 flex'>
+                        {[...Array(Math.round(rating.rate))].map((e, i) => (
+                            <StarIcon
+                                key={`star-${i}`}
+                                className='text-orange-500 w-4'
+                                aria-hidden="true"
+                            />
+                        ))}
+                        {[...Array(5 - Math.round(rating.rate))].map((e, i) => (
+                            <StarIcon
+                                key={`empty-star-${i}`}
+                                className='text-zinc-900/80  w-4'
+                                aria-hidden="true"
+                            />
+                        ))}
+                        ({rating.count})
+                    </p>
+                    <h1 className='text-2xl text-primary py-2'>Tk. {price}</h1>
                 </div>
                 <button className='inline-flex justify-center w-full rounded-md shadow-sm shadow-zinc-900/10 text-sm text-zinc-50 px-2 py-2 bg-orange-500 hover:bg-orange-700'>Add to Cart</button>
             </div>
--- src/pages/Home/Products/Products.jsx
+++ src/pages/Home/Products/Products.jsx
@@ -1,15 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import ProductCard from '../../../components/ui/ProductCard';
+import axios from 'axios';
 
 const Products = () => {
     const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [filterCategory, setFilterCategory] = useState('all');
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products/categories')
             .then(res => res.json())
             .then(data => setCategories(['all', ...data]))
     }, [])
-    console.log(categories);
+
+    useEffect(() => {
+        let url;
+        if (filterCategory !== 'all') {
+            url = `https://fakestoreapi.com/products/category/${filterCategory}`;
+        }
+        else {
+            url = `https://fakestoreapi.com/products`;
+        }
+        if (url) {
+            axios.get(url)
+                .then(res => setProducts(res.data))
+        }
+    }, [filterCategory])
+
+    const handleCategories = (category) => {
+        setFilterCategory(category);
+    }
     return (
         <div className='container mx-auto my-20'>
             <div>
@@ -18,15 +39,14 @@ const Products = () => {
             <hr />
             <div className='py-4 flex justify-center items-center gap-10'>
                 {
-                    categories?.map((category, i) => <button key={i} className='px-4 py-3 capitalize text-2xl hover:text-orange-600 transition duration-200'>{category}</button>)
+                    categories?.map((category, i) => <button key={i} onClick={() => handleCategories(category)} className='px-4 py-3 capitalize text-2xl hover:text-orange-600 transition duration-200'>{category}</button>)
                 }
             </div>
             <hr />
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-20'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                {
+                    products.map((product) => <ProductCard key={product.id} product={product} />)
+                }
             </div>
         </div>
     );
